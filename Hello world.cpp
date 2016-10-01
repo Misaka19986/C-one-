636157039ea31c41ed1d82bf119cd892a22e4f37
Misaka19986 <1657512446@qq.com>
2016-10-01 17:51:06 +0800

    Add files via upload

---
+++ Hello world.cpp
@@ -0,0 +1,14 @@
+//first
+//The last change time: 2016-6-31
+//use language:C
+//IDE:C-free 5.0
+
+#include <stdio.h>
+
+int main (void)
+{
+	printf ("Hello,world.\n");
+	
+	getchar ();
+	return 0;
+}
